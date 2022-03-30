29688687570e66a2e52097d39bdabbfa1e0b316f
Unknown1321 <arenstorffenterprise@gmail.com>
2022-03-30 11:14:49 +0200

    made help

---
+++ src/main/java/com/madspild/controller/HelpController.java
@@ -0,0 +1,12 @@
+package com.madspild.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelpController {
+    @GetMapping("/hjælp")
+    public String getHjælp(){
+        return "hjælp";
+    }
+}
