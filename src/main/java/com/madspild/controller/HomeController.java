8397b70d2b974220860ffd0008fe71017e350210
Mardin <mardineliassi57@gmail.com>
2022-03-30 11:30:20 +0200

    - Nav bar works!

--- src/main/java/com/madspild/controller/HomeController.java
+++ src/main/java/com/madspild/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/index")
+    @GetMapping("/startside")
     public String getIndex(){
         return "index";
     }
--- src/main/java/com/madspild/controller/InfoController.java
+++ src/main/java/com/madspild/controller/InfoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class InfoController {
 
 
-    @GetMapping("/aboutus")
+    @GetMapping("/om-os")
     public String getOmOs(){
         return "aboutus";
     }
