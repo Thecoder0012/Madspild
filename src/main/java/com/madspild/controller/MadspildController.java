a6ffcd151d2730e0e0e310435c9cc537f26d4ab4
Mardin <mardineliassi57@gmail.com>
2022-03-30 12:53:58 +0200

    - Dropdown menu created for madspild!

--- src/main/java/com/madspild/controller/MadspildController.java
+++ src/main/java/com/madspild/controller/MadspildController.java
@@ -6,10 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MadspildController {
 
-
     @GetMapping("/madspild")
     public String getMadspild(){
         return "madspild";
-
     }
 }
