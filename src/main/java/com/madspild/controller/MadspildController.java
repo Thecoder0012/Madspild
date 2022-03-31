27b2049d93a45f7344d4ffa54ba60d334aa3f868
Mardin <mardineliassi57@gmail.com>
2022-03-31 12:18:24 +0200

    - changes

--- src/main/java/com/madspild/controller/MadspildController.java
+++ src/main/java/com/madspild/controller/MadspildController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MadspildController {
 
-    @GetMapping("/madspild")
+    @GetMapping("/hvad-er-madspild")
     public String getMadspild(){
-        return "home/madspild";
+        return "home/what_is_madspild";
     }
 }
