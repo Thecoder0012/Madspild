b287d3e72cd44e368cb75368609901972595e63a
Mardin <mardineliassi57@gmail.com>
2022-04-03 16:07:07 +0200

    - All web-requests are now in HomeController.java

--- src/main/java/com/madspild/controller/HomeController.java
+++ src/main/java/com/madspild/controller/HomeController.java
@@ -7,8 +7,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/startside")
-    public String getIndex(){
+    @GetMapping(value = "/startside")
+    public String getIndex() {
         return "home/index";
+    }
+
+    @GetMapping(value = "/hvad-er-madspild")
+    public String getMadspild() {
+        return "home/what_is_madspild";
+    }
+
+    @GetMapping(value ="/om-os")
+    public String getAboutUs() {
+        return "home/aboutus";
+    }
+
+    @GetMapping(value ="/kontakt")
+    public String getReadMore() {
+        return "home/contact";
+    }
+
+    @GetMapping(value ="/hjælp")
+    public String getHelp() {
+        return "home/help";
+    }
+
+    @GetMapping(value ="/logo")
+    public String getLogo() {
+        return "home/logo";
     }
 }
