0b234571c659782c1be9d65b7f359df42b4d4e85
Mardin <mardineliassi57@gmail.com>
2022-04-07 13:10:19 +0200

    - Css changes

--- src/main/java/com/madspild/controller/HomeController.java
+++ src/main/java/com/madspild/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 @Controller
 public class HomeController {
 
